5c005c21da5ac5603430f115e401fd40c331a835
Olesya <lesa76632@gmail.com>
2026-02-01 21:44:47 +0300

    Add Task data class withenumPriority

---
+++ src/Task.kt
@@ -0,0 +1,9 @@
+﻿data class Task(
+    val title: String,
+    val description: String,
+    val priority: Priority,
+    var isCompleted: Boolean = false
+)
+enum class Priority {
+    LOW, MEDIUM, HIGH
+}
