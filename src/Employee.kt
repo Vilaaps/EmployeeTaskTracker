77edb5818352e19d56ac505adb82ec5c3e4797d1
Olesya <lesa76632@gmail.com>
2026-02-01 21:39:27 +0300

    Implement customsettersforsalary and experience

---
+++ src/Employee.kt
@@ -0,0 +1,39 @@
+﻿class Employee(
+    fullName: String,
+    position: String,
+    salary: Int,
+    yearsOfExperience: Int
+) {
+    private var _fullName: String = fullName
+    private var _position: String = position
+    private var _salary: Int = salary
+    private var _yearsOfExperience: Int = yearsOfExperience
+    val fullName: String
+        get() = _fullName
+    val position: String
+        get() = _position
+    var salary: Int
+        get() = _salary
+        set(value) {
+            if (value < 0) {
+                println("Зп не может быть отрицательной")
+            } else {
+                _salary = value
+            }
+        }
+    var yearsOfExperience: Int
+        get() = _yearsOfExperience
+        set(value) {
+            when {
+                value > 50 -> {
+                    println("Опыт ограничен 50 годами")
+                    _yearsOfExperience = 50
+                }
+                value < 0 -> {
+                    println("Опыт не может быть отрицательным")
+                    _yearsOfExperience = 0
+                }
+                else -> _yearsOfExperience = value
+            }
+        }
+}
---
+++ src/Main.kt
@@ -0,0 +1,13 @@
+﻿fun main() {
+    val employee = Employee(
+        fullName = "Самойлова О.М",
+        position = "Журналист",
+        salary = 140000,
+        yearsOfExperience = 10
+    )
+    println("Сотрудник:")
+    println("ФИО: ${employee.fullName}")
+    println("Должность: ${employee.position}")
+    println("Зарплата: ${employee.salary}")
+    println("Опыт: ${employee.yearsOfExperience}")
+}
